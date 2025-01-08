b2cd73b41e5a4d4d2e7bf1fbb03a0f39507fdb1d
RohitShee <rohitshee92@gmail.com>
2025-01-08 23:48:07 +0530

    write Chat Store context

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { useChatStore } from '../store/useChatStore'
 
 const HomePage = () => {
+  const {selectedUsers} = useChatStore();
   return (
-    <div>
-      <h1>Homepage</h1>
+    <div className='min-h-screen bg-base-200'>
+     <div className='flex items-center justify-center pt-20 px-4'>
+      <div className='bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]'>
+        <div className='flex h-full rounded-lg overflow-hidden'>
+            <Sidebar />
+            {!selectedUsers ? <NoChatSelected/> : <ChatContainer/>}
+        </div>
+      </div>
+     </div>
     </div>
   )
 }
