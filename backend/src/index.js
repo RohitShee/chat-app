7605cefb2616c6a324a4155ee974dc6d66bb94be
RohitShee <rohitshee92@gmail.com>
2025-02-08 23:44:33 +0530

    project complete

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser'
 import cors from 'cors';
+import path from 'path';
 
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
@@ -12,6 +13,7 @@ import { app,server } from './lib/socket.js';
 
 dotenv.config();
 const PORT = process.env.PORT || 8000;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser())
@@ -19,12 +21,22 @@ app.use(cors({
     origin: 'http://localhost:5173',
     credentials : true,
 }))
-server.listen(PORT, ()=>{
-    console.log('Server started on http://localhost:'+PORT);
-    connectDB()
-})
 
 app.use('/api/auth',authRoutes);
 app.use('/api/messages',messageRoutes);
 app.use('/api/user',userRoutes);
 app.use('/api/group',groupRoutes);
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname,'../frontend/dist')));
+
+    app.get('*',(req,res)=>{
+        res.sendFile(path.join(__dirname,'../frontend','dist','index.html'))
+    })
+}
+
+server.listen(PORT, ()=>{
+    console.log('Server started on http://localhost:'+PORT);
+    connectDB()
+})
+
